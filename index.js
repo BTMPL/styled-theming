6c5eeb5d88ebd819c54d5a1716a7a0128376cbca
Bartosz Szczecinski <baphemot@gmail.com>
2018-02-08 11:17:37 +0100

    Implemented the changed logic, removed flow

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-'use strict';
+var getCombinations = function(set, props) {
+  var results = (function acc(xs, set) {
+    var x = xs[0];
+    if (typeof x === "undefined") {
+      return set;
+    }
+
+    for (var i = 0, l = set.length; i < l; ++i) {
+      if (props[x]) {
+        set.push(set[i].concat(props[x]));
+      }
+    }
+
+    return acc(xs.slice(1), set);
+  })(set, [[]]).slice(1);
+
+  return results.reduce(function acc(accumulator, item) {
+    return accumulator.concat(itme.join("."))
+  }, []);
+};
 
 function getThemeValue(name, props, values) {
-  var value = (
-    props.theme &&
-    props.theme[name]
-  );
+  var value = props.theme && props.theme[name];
 
   var themeValue;
 
-  if (typeof value === 'function') {
+  if (typeof value === "function") {
     themeValue = value(values);
   } else {
     themeValue = values[value];
   }
 
-  if (typeof themeValue === 'function') {
+  if (typeof themeValue === "function") {
     return themeValue(props);
   } else {
     return themeValue;
@@ -29,9 +45,25 @@ function theme(name, values) {
 
 theme.variants = function(name, prop, values) {
   return function(props) {
-    var variant = props[prop] && values[props[prop]];
-    return variant && getThemeValue(name, props, variant);
+    if (Array.isArray(prop)) {
+      var result = getCombinations(
+        prop,
+        props
+      ).reduce(function (accumulator, item) {
+        var variant = values[item];
+        if (variant) {
+          var gtv = getThemeValue(name, props, variant) || [];
+          return accumulator.concat(gtv);
+        }
+        return accumulator;
+      }, []);
+
+      return result;
+    } else {
+      var variant = props[prop] && values[props[prop]];
+      return variant && getThemeValue(name, props, variant);
+    }
   };
 };
 
-module.exports = theme;
+export default theme;
